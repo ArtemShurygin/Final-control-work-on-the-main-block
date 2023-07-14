6b149b74af0fb3d452c480fd6da0cb3b595eecef
Artem <shurtemm@mail.ru>
2023-07-14 17:19:58 +0300

    Добавили защиту от ввода другого символа вместо числа (параметр количество строк)

--- Program.cs
+++ Program.cs
@@ -6,33 +6,44 @@
 */
 
 int j = 0;
+int n = 0;
 
 Console.WriteLine("Введите количество элементов (строк):");
-int n = Convert.ToInt32(Console.ReadLine());
-string [] input_array = new string [n];
-string [] output_array = new string [n];
+string input = Convert.ToString(Console.ReadLine());
+if (int.TryParse (input, out int number)) {
+    Console.WriteLine();
+    n = Convert.ToInt32(input);
+    string [] input_array = new string [n];
+    string [] output_array = new string [n];
 
-for (int i=0; i<n; i++) {
-    Console.WriteLine($"Введите  элемент (строку) №{i+1}:");
-    input_array [i] = Convert.ToString(value: Console.ReadLine());
-    if (input_array[i].Length < 4) {
-        output_array [j] = input_array [i];
-        j++;
+    for (int i=0; i<n; i++) {
+        Console.WriteLine($"Введите  элемент (строку) №{i+1}:");
+        input_array [i] = Convert.ToString(value: Console.ReadLine());
+        if (input_array[i].Length < 4) {
+            output_array [j] = input_array [i];
+            j++;
+        }
     }
-}
 
-Console.WriteLine();
-Console.WriteLine("Исходный массив: ");
-for (int i=0; i<n; ++i) {
-    Console.WriteLine(input_array [i]);
-}
+    Console.WriteLine();
+    Console.WriteLine("Исходный массив: ");
+    for (int i=0; i<n; ++i) {
+        Console.WriteLine(input_array [i]);
+    }
 
-Console.WriteLine();
-if (j==0) {
-    Console.WriteLine("Нет строк, длина которых меньше, либо равна 3 символам: ");
+    Console.WriteLine();
+    if (j==0) {
+        Console.WriteLine("Нет строк, длина которых меньше, либо равна 3 символам: ");
+    }
+    else {
+        Console.WriteLine("Строки, длина которых меньше, либо равна 3 символам: ");
+        for (int i=0; i<j; i++) 
+        Console.WriteLine(output_array [i]);
+    }
+
 }
 else {
-    Console.WriteLine("Строки, длина которых меньше, либо равна 3 символам: ");
-    for (int i=0; i<j; i++) 
-    Console.WriteLine(output_array [i]);
+    Console.WriteLine();
+    Console.WriteLine("Ошибка: в параметре \"Количество элементов (строк)\" задано не число, а иной символ");
 }
+
