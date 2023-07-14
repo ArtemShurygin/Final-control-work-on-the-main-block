5f80aad794fd7113164548788ba49e7c930ace81
Artem <shurtemm@mail.ru>
2023-07-14 16:27:12 +0300

    решили задачу

--- Program.cs
+++ Program.cs
@@ -5,20 +5,34 @@
 лучше обойтись исключительно массивами.
 */
 
+int j = 0;
+
 Console.WriteLine("Введите количество элементов (строк):");
 int n = Convert.ToInt32(Console.ReadLine());
 string [] input_array = new string [n];
 string [] output_array = new string [n];
 
-for (int i=0; i<n; ++i) {
+for (int i=0; i<n; i++) {
     Console.WriteLine($"Введите  элемент (строку) №{i+1}:");
     input_array [i] = Convert.ToString(value: Console.ReadLine());
-    Console.WriteLine(input_array [i]);
+    if (input_array[i].Length < 4) {
+        output_array [j] = input_array [i];
+        j++;
     }
-    
+}
+
 Console.WriteLine();
 Console.WriteLine("Исходный массив: ");
-
 for (int i=0; i<n; ++i) {
     Console.WriteLine(input_array [i]);
-    }
+}
+
+Console.WriteLine();
+if (j==0) {
+    Console.WriteLine("Нет строк, длина которых меньше, либо равна 3 символам: ");
+}
+else {
+    Console.WriteLine("Строки, длина которых меньше, либо равна 3 символам: ");
+    for (int i=0; i<j; i++) 
+    Console.WriteLine(output_array [i]);
+}
